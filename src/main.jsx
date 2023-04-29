ca3adb2c6d69c4b39a73ce1dbf20e9b658d398a8
Clara <clarapachecodev@gmail.com>
2023-04-29 19:49:18 -0300

    Import ThemeProvider from styled-component, import theme from theme.js,importGlobalStyles from global.js, correct named import for App componenent and use all of those components to build main.jsx component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Pages/App.jsx'
+
+import { ThemeProvider } from 'styled-components'
+import theme from './Styles/theme.js'
+import GlobalStyles from './Styles/global.js'
+
+import { App } from './Pages/App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
