482ee0c22507b8d267c9609a5762cecd3fb13691
Clara <clarapachecodev@gmail.com>
2023-04-30 09:56:50 -0300

    Create MovieInfo Container css component with styles for img and p elements

---
+++ src/Components/MovieInfo/MovieInfo.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 600px;
+  height: 600px;
+
+  margin-top: 180px;
+
+  border: 2px solid blue;
+
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-items: center;
+
+  > img {
+    width: 245px;
+    height: 245px;
+
+    margin-bottom: 30px;
+
+  }
+
+  > p {
+    font-size: 1.2rem;
+    margin-bottom: 20px;
+    text-align: justify;
+  }
+
+
+
+`;
