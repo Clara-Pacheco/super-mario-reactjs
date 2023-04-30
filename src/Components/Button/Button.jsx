b5088bf18a63aebc2ef3070a7a0da4ee85d4ab66
Clara <clarapachecodev@gmail.com>
2023-04-30 15:29:00 -0300

    Import userEffect from react and Aos animation from aos, create an effectand apply it to the component Button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from 'react'
+import Aos from "aos"
+import "aos/dist/aos.css"
+
 import { Container } from './Button.js'
 
 export function Button({text, isActive=false, url}){
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
-    <Container isActive={isActive}>
-      <a 
+    <Container
+       isActive={isActive}  
+       data-aos="fade-up"
+       data-aos-delay="600"
+       data-aos-duration="800"
+       data-aos-easing="ease-in-out"  
+    >
+       
+      <a  
         href={url}
         target='_blank'
         rel='noreferrer'
