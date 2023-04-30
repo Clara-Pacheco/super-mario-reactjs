7ada2a19fbf37de2fa4a61d63b228333e70f4afd
Clara <clarapachecodev@gmail.com>
2023-04-30 15:27:22 -0300

    Remove importation of useEffect and aos lib

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+
 import { ThemeProvider } from 'styled-components'
 import theme from './Styles/theme.js'
 import GlobalStyles from './Styles/global.js'
