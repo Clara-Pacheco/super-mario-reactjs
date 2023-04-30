884604107c6c5c61b6288fc3806aff7613021c35
Clara <clarapachecodev@gmail.com>
2023-04-30 13:37:32 -0300

    Change Button css Container dimensions, change border-radius, add transform skew and add styles for font

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-  width: 230px;
-  height: 70px;
+
+  width: 280px;
+  padding: 10px 0;
+  margin-right: 25px;
 
   background-color: ${({ theme,isActive }) => isActive ? theme.COLORS.RED:'transparent'};
   border: 1px solid ${({ theme, isActive}) => isActive ? theme.COLORS.RED: theme.COLORS.GREEN};
-  border-radius: 5px;
+  border-radius: 3px;
 
-  margin-left: 30px;
+  transform: skew(-10deg);
+
+  font-size: 16px;
+  font-weight: 600;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 0.3px;
 `;
