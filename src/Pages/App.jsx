4582d98af16d57b200d268a704091ed50eaf78cd
Clara <clarapachecodev@gmail.com>
2023-04-30 12:26:53 -0300

    Import Button component and use it in the composition of App component passing the values of text and isActive for props

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -6,6 +6,7 @@ import marioAndLuigi from '../assets/mario-and-luigi.png'
 
 import { Header } from '../Components/Header/Header.jsx'
 import { MovieInfo } from '../Components/MovieInfo/MovieInfo.jsx'
+import { Button } from '../Components/Button/Button.jsx'
 
 export function App() {
  return (
@@ -19,17 +20,28 @@ export function App() {
       <source src={backgroundVideo} type="video/mp4" />
     </Video>
     <main>
-      <MovieInfo
-        imgUrl={movieLogo}
-        altText='Mario Bross Movie Logo'
-        text='Um dia, Mario e Luigi vão para no reino dos
-         cogumelos, governado pela Princesa Peach, mas
-         ameaçado pelo rei dos Koopas, Bowser, que vai
-         fazer de tudo para conseguir reinar todos os lugares.
-         É então quando Luigi é raptado por Bowser e o usa para
-         procurar Mario, o único capaz de deter o Koopa e
-         reestabelexcer a paz.'
-      />
+      <div>
+        <MovieInfo
+          imgUrl={movieLogo}
+          altText='Mario Bross Movie Logo'
+          text='Um dia, Mario e Luigi vão para no reino dos
+          cogumelos, governado pela Princesa Peach, mas
+          ameaçado pelo rei dos Koopas, Bowser, que vai
+          fazer de tudo para conseguir reinar todos os lugares.
+          É então quando Luigi é raptado por Bowser e o usa para
+          procurar Mario, o único capaz de deter o Koopa e
+          reestabelexcer a paz.'
+        />
+       
+          <Button
+            text='Assistir o Trailer'
+            isActive
+          />
+          <Button
+            text='Comprar Ingresso'
+          />
+          
+        </div>
 
         <img 
           src={marioAndLuigi}
