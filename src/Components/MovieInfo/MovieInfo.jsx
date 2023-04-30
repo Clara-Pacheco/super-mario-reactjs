3bc170fa8cd7b6622fb7e6f58a7336b06d542c5f
Clara <clarapachecodev@gmail.com>
2023-04-30 15:31:01 -0300

    Import useEffect from react, Aos from aos lib, create and effect and apply it to img and p elements

--- src/Components/MovieInfo/MovieInfo.jsx
+++ src/Components/MovieInfo/MovieInfo.jsx
@@ -1,14 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from 'react'
+import Aos from "aos"
+import "aos/dist/aos.css"
+
 import { Container } from './MovieInfo.js'
 
 export function MovieInfo({ imgUrl, altText, text}) {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <Container>
       <img 
+        data-aos="fade-up"
+        data-aos-duration="800"
+        data-aos-easing="ease-in-out"
         src={imgUrl}
         alt={altText}
       />
-      <p>{text}</p>
+      <p
+        data-aos="fade-up"
+        data-aos-delay="300"
+        data-aos-duration="800"
+        data-aos-easing="ease-in-out"
+      >
+        {text}
+
+      </p>
     </Container>
   )
 }
