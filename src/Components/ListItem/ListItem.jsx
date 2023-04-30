05987fa13e768c95e82c26d1f5ef644e819aefcb
Clara <clarapachecodev@gmail.com>
2023-04-30 11:10:23 -0300

    Destructure url prop and add it to href attribute from anchor element

--- src/Components/ListItem/ListItem.jsx
+++ src/Components/ListItem/ListItem.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import { Container } from './ListItem.js'
 
-export function ListItem({ menuItem }){
+export function ListItem({ menuItem, url }){
   return (
     <Container>
       <a
-       href='#'
+       href={url}
        target="_blank"
+       rel='noreferrer'
       >
        {menuItem}
       </a>
