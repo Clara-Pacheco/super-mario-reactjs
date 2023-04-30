f8a57f2956a2a93402e262fed4586bcb10dd3001
Clara <clarapachecodev@gmail.com>
2023-04-30 12:44:53 -0300

    Transform Button component in a link passing url as prop

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
 import { Container } from './Button.js'
 
-export function Button({text, isActive=false}){
+export function Button({text, isActive=false, url}){
   return (
     <Container isActive={isActive}>
+      <a 
+        href={url}
+        target='_blank'
+        rel='noreferrer'
+      >
       {text}
+      </a>
     </Container>
   )
 }
