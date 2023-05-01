934f6f4a5f40ea79f946bec24c385e2a46dc08f5
Clara <clarapachecodev@gmail.com>
2023-05-01 12:08:53 -0300

    Add media query for MovieInfo component for screens max-width 500px

--- src/Components/MovieInfo/MovieInfo.js
+++ src/Components/MovieInfo/MovieInfo.js
@@ -11,17 +11,32 @@ export const Container = styled.div`
   align-items: start;
   justify-items: center;
 
+  @media (max-width: 500px) {
+          width: 300px;
+          height: 250px;
+
+          margin: 160px auto 30px;
+          width: 300px;
+          height: 300px;
+
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-items: center;
+      
+        }
+
+
   > img {
     width: 280px;
-    height: 230px;
+    height: 450px;
 
     margin-bottom: 30px;
-
   }
 
   > p {
-    font-size: 1.2rem;
-    margin-bottom: 20px;
+    font-size: 1.3rem;
+    margin-bottom: 50px;
     text-align: justify;
   }
 
