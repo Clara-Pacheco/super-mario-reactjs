9cd4f9c48346cf811f43b09748ee06b712dee0d2
Clara <clarapachecodev@gmail.com>
2023-05-02 08:24:13 -0300

    Remove anchor element from Button component construction

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -11,20 +11,21 @@ export function Button({text, isActive=false, url}){
   }, []);
   return (
     <Container
+       url={url}
        isActive={isActive}  
        data-aos="fade-up"
        data-aos-delay="600"
        data-aos-duration="800"
        data-aos-easing="ease-in-out"  
     >
-       
-      <a  
+        {text}
+      {/* <a  
         href={url}
         target='_blank'
         rel='noreferrer'
       >
       {text}
-      </a>
+      </a> */}
     </Container>
   )
 }
