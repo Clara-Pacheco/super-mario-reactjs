a22ca8f4bc6fbfbd42919d8bd1d0f28303005f87
Clara <clarapachecodev@gmail.com>
2023-05-02 10:45:53 -0300

    Add nav element to Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,56 +1,67 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100wv;
-  height: 100px;
 
-  padding-left: 150px;
-  padding-right: 150px;
-
-  list-style: none;
-
-  @media (max-width: 500px) {
-    display: none;
-  }
-  @media (min-width: 501px) {
-    width: 100wv;
-    height: 100px;
-
-    padding: 30px;
-
-    list-style: none;
-
-  }
-  @media (min-width: 1200px){
+  > nav {
     width: 100wv;
     height: 100px;
 
     padding-left: 150px;
     padding-right: 150px;
+
+    list-style: none;
+
+    @media (max-width: 500px) {
+      display: none;
+    }
+    @media (min-width: 501px) {
+      width: 100wv;
+      height: 100px;
+
+      padding: 30px;
+
+      list-style: none;
+
+    }
+    @media (min-width: 1200px){
+      width: 100wv;
+      height: 100px;
+
+      padding-left: 150px;
+      padding-right: 150px;
+        
+      }
+
+
+    > div {
+      display: flex;
+      align-items: center;
       
+
+      > img {
+        width: 80px;
+        margin-right: 40px;
+
+        @media (max-width: 500px) {
+          width: 50px;
+          margin-right: 35px; 
+          margin-left: 20px;   
+      }
+
+      }
     }
 
-
-  > div {
+    > div {
+      > button {
+        @media (max-width: 1200px) {
+        visibility: visible;
+        opacity: 1;
+      }
+      }
+    }
     display: flex;
     align-items: center;
-    
+    justify-content: space-between;
 
-    > img {
-      width: 80px;
-      margin-right: 40px;
-
-      @media (max-width: 500px) {
-        width: 50px;
-        margin-right: 35px; 
-        margin-left: 20px;   
-  }
-
-    }
-  }
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
+    }     
 `;
